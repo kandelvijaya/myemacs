915325fbea2396c918654711e61c89076042c702
kandelvijaya <vijaya.prakash.kandel@zalando.de>
2022-09-03 12:44:41 +0200

    Added 3 padding on left to Org Mode, added package-priorities

--- init.el
+++ init.el
@@ -4,27 +4,38 @@
 (tooltip-mode -1)                 ;; tooltip is a overlay info, disable it 
 (set-fringe-mode 10)              ;; The fringe is a thin strip on the left and/or right edge of a window. 
 (menu-bar-mode -1)                ;; disable menu bar mode
-(toggle-frame-fullscreen)         ;; start with fullscreen 
+(toggle-frame-fullscreen)         ;; start with fullscreen
 
 ;; download Fira code fonts which looks good. 
 (set-face-attribute 'default nil :font "Fira Code" :height 150)
 
 
-;; use M-x eval-buffer to run this.
-;; use M-x describe-function to get help
 
+;; Customizing ModeLine with all-the-icons
+(use-package all-the-icons
+  :if (display-graphic-p))
 
 ;; Make ESC key work like CRTL + G, or hide log/warning prompts 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-
 ;; =========== Packagae loading and sources ===================
 (require 'package)
 
+;; Deprioritize Melpa over all stable archives if same package is found on stable versions. 
 (setq package-archives '(("melpa-stable" . "https://stable.melpa.org/packages/")
 			 ("org" . "https://orgmode.org/elpa")
 			 ("elpa" . "https://elpa.gnu.org/packages/")
-			 ))
+			 ("melpa" . "https://melpa.org/packages/")
+			 )
+      package-arch
+      package-archive-priorities '(
+				   ("melpa-stable". 10)
+				   ("org". 9)
+				   ("elpa". 8)
+				   ("melpa". 1)
+				   )
+      )
+
 
 (package-initialize)
 
@@ -36,11 +47,6 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
-
-;; package to show command being used and what the function it calls
-; (use-package command-log-mode)
-;; didn't work
-
 
 ;; line and column numbers show
 (column-number-mode)
@@ -55,7 +61,6 @@
 		eshell-mode-hook))
 	 (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-
 ;; Dark theme. Non-dark variant is found with spacemacs-light
 ;; alternative good option is tango-dark.
 ;; unsing t at the end supresses interactive prompt.
@@ -63,29 +68,12 @@
   :defer t
   :init (load-theme 'spacemacs-dark t))
 
-
-;; Customizing ModeLine
-(use-package all-the-icons
-  :if (display-graphic-p))
-
 ;; if all-icons-font is not installed prompt the user to install
 ;; manula installation via `M-x all-the-icons-install-fonts`
 (use-package doom-modeline
   :ensure t
   :hook (after-init . doom-modeline-mode)
   :custom (doom-modeline-height 15))
-
-
-;; magit
-(use-package magit)
-
-
-;; Displays all associated key binding and their functions after set delay on the mini-mode
-(use-package which-key
-  :init(which-key-mode)
-  :diminish
-  :config
-  (setq which-key-idle-delay 2))
 
 ;; fuzzy search on M-x for use with Selectrum
 ;; for example: `M-x swi buf` to get to Switch Buffer
@@ -112,30 +100,35 @@
   ;; enabled right away. Note that this forces loading the package.
   (marginalia-mode))
 
-
+;; Displays all associated key binding and their functions after set delay on the mini-mode
+(use-package which-key
+  :init(which-key-mode)
+  :diminish
+  :config
+  (setq which-key-idle-delay 2))
 
 ;; ORG-mode specifics
-(use-package org)
+(use-package org
+  :config
+  (setq org-ellipsis " ▼")
+  )
+
+(defun bj/set-left-padding ()
+     (set-window-margins (selected-window) 3 0)
+  )
+
+;; prettify bullets
+(use-package org-bullets
+  :after org
+  :hook
+  (org-mode . org-bullets-mode)
+  (org-mode . bj/set-left-padding)
+  )
+
+;; use org-tempo to allow faster code block addition
+(require 'org-tempo)
+
 (use-package org-superstar)
-
-
-;; Writing specifics
-(use-package writeroom-mode)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
- '(package-selected-packages
-   '(emacsql-sqlite3 org-roam writeroom-mode org-superstar marginalia consult selectrum-prescient which-key use-package spacemacs-theme magit doom-modeline counsel all-the-icons)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;; This is required for org-roam to not error on database connection. 
 (use-package emacsql-sqlite3)
@@ -147,19 +140,57 @@
   :custom
   (org-roam-directory (file-truename "~/roamnotes/"))
   :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n g" . org-roam-graph)
-         ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         ;; Dailies
-         ("C-c n j" . org-roam-dailies-capture-today))
+	 ("C-c n f" . org-roam-node-find)
+	 ("C-c n g" . org-roam-graph)
+	 ("C-c n i" . org-roam-node-insert)
+	 ("C-c n c" . org-roam-capture)
+	 ;; Dailies
+	 ("C-c n j" . org-roam-dailies-capture-today))
   :config
   (org-roam-setup)
   )
 
+;; Zen Mode Writing
+(use-package writeroom-mode)
 
-;; magit
+;; magit setup
 (use-package magit)
 
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t))
+ )
 
+(defun org-babel-tangle-emacsconfig-on-save ()
+    (when (string-equal (buffer-file-name)
+			(expand-file-name "~/.emacs.d/EmacsConfig.org"))
+      (let ((org-config-babel-evaluate nil))
+	(org-babel-tangle))))
 
+  (add-hook 'org-mode-hook
+	    (lambda ()
+	      (add-hook 'after-save-hook #'org-babel-tangle-emacsconfig-on-save)))
+
+;;; uses tab to show completions afte the thing is indented
+(setq tab-always-indent 'complete)
+(add-to-list 'completion-styles 'initials t)
+
+;; (defvar bootstrap-version)
+;; (let ((bootstrap-file
+;;        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+;;       (bootstrap-version 6))
+;;   (unless (file-exists-p bootstrap-file)
+;;     (with-current-buffer
+;; 	(url-retrieve-synchronously
+;; 	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+;; 	 'silent 'inhibit-cookies)
+;;       (goto-char (point-max))
+;;       (eval-print-last-sexp)))
+;;   (load bootstrap-file nil 'nomessage))
+
+;; (setq package-enable-at-startup nil)
+
+(use-package org-roam-ui)
+
+;; required for org-roam-export to be available
+(require 'org-roam-export)
